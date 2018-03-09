4ec66deab02d939ab40ad7a70f54c53928d75a39
Jose Luis T <java.mx@gmail.com>
2018-03-09 00:48:37 -0300

    about creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BodyComponent } from './components/body/body.component';
+import { AboutComponent } from './components/about/about.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { BodyComponent } from './components/body/body.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
