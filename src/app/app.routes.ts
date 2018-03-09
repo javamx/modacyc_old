969ee8bc8eabbea53e29398c96fa4f84aa1c92d7
Jose Luis T <java.mx@gmail.com>
2018-03-09 01:31:53 -0300

    rutas agregadas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from '@angular/router';
+import {
+    AboutComponent,
+    ItemsComponent,
+    BodyComponent
+} from "./components/index.paginas"
+
+const app_routes: Routes = [
+    { path: '', component: BodyComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'items', component: ItemsComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+
+export const app_routing = RouterModule.forRoot(app_routes, {useHash:true});
---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,5 @@
+
+
+export { AboutComponent } from "./about/about.component";
+export { BodyComponent } from "./body/body.component";
+export { ItemsComponent } from "./items/items.component";
