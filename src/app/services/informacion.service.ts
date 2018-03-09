900ab186310c0e1e097b6fc63d70c196ad0958ca
Jose Luis T <java.mx@gmail.com>
2018-03-09 03:43:52 -0300

    Comienzo con firebase, equipo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,19 +4,33 @@ import { Http } from "@angular/http";
 @Injectable()
 export class InformacionService {
 
-  info:any = {};
-  cargada:boolean = false;
+  info: any = {};
+  cargada: boolean = false;
+  cargada_sobre_nosotros: boolean = false;
+  equipo:any[] = [];
 
-  constructor( public http:Http ) { 
+  constructor(public http: Http) {
 
+    this.carga_info();
+    this.carga_sobre_nosotros();
+
+  }
+
+  public carga_info() {
     this.http.get("assets/data/info.pagina.json")
-              .subscribe( data =>{
-                console.log(data.json());
-                this.cargada = true;
-                this.info = data.json();
-              })
+      .subscribe(data => {
+        this.cargada = true;
+        this.info = data.json();
+      })
 
-
+  }
+  public carga_sobre_nosotros() {
+    this.http.get("https://modacyc-45e03.firebaseio.com/equipo.json")
+      .subscribe(data => {
+        console.log(data.json());
+        this.cargada_sobre_nosotros = true;
+        this.equipo = data.json();
+      })
 
   }
 
