e3049cf017c923dffb4f050e9541f9d36013aab6
Jose Luis T <java.mx@gmail.com>
2018-03-11 00:25:31 -0300

    busqueda creada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { RouterModule, Routes } from '@angular/router';
 import {
     AboutComponent,
     ItemsComponent,
-    BodyComponent
+    BodyComponent,
+    SearchComponent
 } from "./components/index.paginas"
 
 const app_routes: Routes = [
     { path: '', component: BodyComponent },
     { path: 'about', component: AboutComponent },
     { path: 'items/:id', component: ItemsComponent },
+    { path: 'buscar/:termino', component: SearchComponent },
     { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
--- src/app/components/index.paginas.ts
+++ src/app/components/index.paginas.ts
@@ -3,3 +3,4 @@
 export { AboutComponent } from "./about/about.component";
 export { BodyComponent } from "./body/body.component";
 export { ItemsComponent } from "./items/items.component";
+export { SearchComponent } from "./search/search.component";
